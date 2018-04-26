8d513f32d144cc5d32427f62ff36a33c69122d9b
Scott Mebberson <scott@scottmebberson.com>
2018-04-26 15:51:16 +0930

    Added the c hosts remove command.

--- bin/c-hosts.js
+++ bin/c-hosts.js
@@ -8,6 +8,7 @@ const { missingCommand } = require('./lib/c');
 // The basic program, which uses sub-commands.
 program
     .command('add <ip> <domain>', 'A a record to /etc/hosts')
+    .command('remove <ip> <domain>', 'Remove a record from /etc/hosts')
     .parse(process.argv);
 
 missingCommand(program);
