2ffde57d2c143ff559701b05e9b827d7e4bc4d7e
Scott Mebberson <scott@scottmebberson.com>
2018-04-27 11:09:32 +0930

    Added the c yarn proxy command.

---
+++ bin/c-yarn.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const { missingCommand } = require('./lib/c');
+
+// The basic program, which uses sub-commands.
+program
+    .command('proxy <location> [cmd...]', 'Run a Yarn command against an NPM location.')
+    .parse(process.argv);
+
+missingCommand(program);
