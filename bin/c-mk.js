ddbc633b74b2b05d829a631cdb671634f48003a4
Scott Mebberson <scott@scottmebberson.com>
2018-04-27 09:45:32 +0930

    Added the c mk delete command.

--- bin/c-mk.js
+++ bin/c-mk.js
@@ -7,6 +7,7 @@ const { missingCommand } = require('./lib/c');
 
 // The basic program, which uses sub-commands.
 program
+    .command('delete', 'Delete minikube')
     .command('hosts', 'Add Minikube\'s IP to your hosts record')
     .command('ip', 'Retrieve Minikube\'s IP')
     .command('restart', 'Restart minikube')
