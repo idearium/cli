31cca670bfe8febde4a5a834ba445f4b59ba67e5
Scott Mebberson <scott@scottmebberson.com>
2018-04-27 14:32:17 +0930

    Added c project env ls command.

--- bin/c-project-env.js
+++ bin/c-project-env.js
@@ -8,6 +8,7 @@ const { missingCommand } = require('./lib/c');
 program
     .command('set <env>', 'Configures the active environment.')
     .command('get', 'Gets the current active environment.')
+    .command('ls', 'Lists the defined project environments.')
     .parse(process.argv);
 
 missingCommand(program);
