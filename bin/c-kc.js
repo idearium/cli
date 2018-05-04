92650a0c542ea65cf4f14e1717a5beec482044a7
Scott Mebberson <scott@scottmebberson.com>
2018-05-04 12:11:51 +0930

    Added the c kc pod command.

--- bin/c-kc.js
+++ bin/c-kc.js
@@ -13,6 +13,7 @@ program
     .command('context', 'Get and set the kubectrl context.')
     .command('deploy <location>', 'Update all instances of a Docker location to the latest tag.')
     .command('logs', 'View all logs from Kubernetes pods.')
+    .command('pod <location>', 'Get the name of a pod for a Kubernetes location.')
     .command('start', 'Deploy all Kubernetes locations.')
     .command('stop', 'Stop and remove certain Kubernetes objects described in Kubernetes locations.')
     .description('Helps run kubectl commands. Most commands run using the project\'s context, not kubectrl\'s context.')
