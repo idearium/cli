062549bb26839aa91ff99e8295c7d80e8544d5c0
Scott Mebberson <scott@scottmebberson.com>
2018-05-15 09:29:52 +0930

    Updated c kc logs to respect project context and namespace.

--- bin/c-kc-logs.js
+++ bin/c-kc-logs.js
@@ -2,7 +2,10 @@
 'use strict';
 
 const program = require('commander');
+const getPropertyPath = require('get-value');
 const { exec } = require('shelljs');
+const { loadConfig, loadState } = require('./lib/c');
+const { formatProjectPrefix } = require('./lib/c-project');
 
 program
     .arguments('<selector>')
@@ -12,4 +15,18 @@ program
 // Default the selector.
 const [selector = 'svc'] = program.args;
 
-exec(`stern --color always --selector ${selector}`);
+return Promise.all([
+    loadState('env'),
+    loadConfig(),
+])
+    .then(([env, config]) => {
+
+        const { kubernetes, project } = config;
+        const { organisation, name } = project;
+        const { environments } = kubernetes;
+
+        const namespace = getPropertyPath(config, `kubernetes.environments.${env}.namespace`) || formatProjectPrefix(organisation, name, env, true, true);
+
+        exec(`stern --color always --selector ${selector} --namespace ${namespace} --context ${environments[env].context}`);
+
+    });
