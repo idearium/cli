be2f2c19e3432bbe6174e0578a18c4ec0564508b
Scott Mebberson <scott@scottmebberson.com>
2018-05-16 12:03:54 +0930

    Created c kc docker-env to spawn a new shell setup for Minikubes Docker daemon.

---
+++ bin/c-mk-docker-env.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const os = require('os');
+const program = require('commander');
+const execa = require('execa');
+const { spawn } = require('child_process');
+
+// The basic program, which uses sub-commands.
+program
+    .description('Spawns a login shell with your current environment and additional Docker environment variables to use the Minikube\'s Docker daemon')
+    .parse(process.argv);
+
+Promise.all([
+    // The Docker environment variables we need
+    execa.shell('minikube docker-env'),
+    // The current shell's environment
+    execa.shell('env'),
+])
+    .then(([{ stdout: minikubeStdOut }, { stdout: envStdOut }]) => {
+
+        // Format the Docker environment variables with the current shell's variables
+        const shellExports = minikubeStdOut
+            .split(os.EOL)
+            .filter(line => line.indexOf('export') === 0)
+            .map(line => line.substr(7).replace(/"/g, ''))
+            .concat(envStdOut.split(os.EOL));
+
+        // Turn the environment variables into an object
+        const env = {};
+
+        shellExports.forEach((line) => {
+
+            const [name, value] = line.split('=');
+
+            env[name] = value;
+
+        });
+
+        // Create our new login shell
+        spawn('/bin/bash', ['--login'], {
+            env,
+            stdio: 'inherit',
+        });
+
+    });
--- bin/c-mk.js
+++ bin/c-mk.js
@@ -8,6 +8,7 @@ const { missingCommand } = require('./lib/c');
 // The basic program, which uses sub-commands.
 program
     .command('delete', 'Delete minikube')
+    .command('docker-env', 'Setup the docker command to use Minikube\'s Docker daemon')
     .command('hosts', 'Add Minikube\'s IP to your hosts record')
     .command('ip', 'Retrieve Minikube\'s IP')
     .command('restart', 'Restart minikube')
