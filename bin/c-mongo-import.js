279150b56d30fc3dd07259444b6c901e2599c449
Allan Chau <allan.chau@icloud.com>
2018-06-26 11:22:22 +0930

    Prevent index errors when importing

--- bin/c-mongo-import.js
+++ bin/c-mongo-import.js
@@ -35,7 +35,7 @@ loadConfig('mongo')
             return reportError(new Error(`Could not find the ${env} db`), program);
         }
 
-        return spawn(`docker run -it -v ${process.cwd()}/data/${db.name}:/data/${db.name} --add-host ${localDb.host}:$(c hosts get -n ${localDb.host}) --rm mongo:latest mongorestore --drop -h ${localDb.host}:${localDb.port} -d ${localDb.name} data/${db.name}`, {
+        return spawn(`docker run -it -v ${process.cwd()}/data/${db.name}:/data/${db.name} --add-host ${localDb.host}:$(c hosts get -n ${localDb.host}) --rm mongo:latest mongorestore --noIndexRestore --drop -h ${localDb.host}:${localDb.port} -d ${localDb.name} data/${db.name}`, {
             shell: true,
             stdio: 'inherit',
         });
