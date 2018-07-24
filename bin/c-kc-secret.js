52754085453e5d6cbef89f93126726163cc160f0
Allan Chau <allan.chau@icloud.com>
2018-07-24 15:58:12 +0930

    Add ability to decode a secret

--- bin/c-kc-secret.js
+++ bin/c-kc-secret.js
@@ -7,6 +7,7 @@ const { newline, reportError } = require('./lib/c');
 
 program
     .arguments('<secret>')
+    .option('-d', 'Decrypt the string.')
     .option('-n', 'Do not print the trailing newline character.')
     .description('Base64 encode a string ready for a Kubernetes secret.')
     .parse(process.argv);
@@ -17,6 +18,9 @@ if (!secret) {
     return reportError(new Error('You need to provide a secret'), program, true);
 }
 
+const decoded = secret.toString('base64');
 const encoded = Buffer.from(secret).toString('base64');
 
-process.stdout.write(`${encoded}${newline(program.N)}`);
+const cmd = program.D ? decoded : encoded;
+
+process.stdout.write(`${cmd}${newline(program.N)}`);
