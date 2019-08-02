cf3a0e03a6f0d50ef00fef351ef261e79c45a88d
smebberson <scott@scottmebberson.com>
2019-08-02 16:06:04 +0930

    Updated c kc build to accept a tag.

--- bin/c-kc-build.js
+++ bin/c-kc-build.js
@@ -4,93 +4,109 @@
 
 const program = require('commander');
 const { exec } = require('shelljs');
-const { dockerToKubernetesLocation, loadConfig, loadState, storeState } = require('./lib/c');
+const {
+    dockerToKubernetesLocation,
+    loadConfig,
+    loadState,
+    storeState,
+} = require('./lib/c');
 const getPropertyPath = require('get-value');
 
 program
     .arguments('<location>')
     .option('-d', 'Also deploy the location(s).')
-    .description('Provide a Docker location and the Dockerfile will be used to build a Docker image. If you don\'t pass a location, all locations will be built.')
+    .option('-t --tag <tag>', 'A tag for the generated Docker image(s).')
+    .description(
+        "Provide a Docker location and the Dockerfile will be used to build a Docker image. If you don't pass a location, all locations will be built.",
+    )
     .parse(process.argv);
 
 const [location = 'all'] = program.args;
+const { tag = String(Math.floor(Date.now() / 1000)) } = program;
 
-return loadConfig()
-    .then((config) => {
+return (
+    loadConfig()
+        .then((config) => {
+            const prefix = exec('c project prefix -n', { silent: true }).stdout;
 
-        const prefix = exec('c project prefix -n', { silent: true }).stdout;
+            return Promise.all([prefix, config]);
+        })
+        .then(([prefix, config]) => {
+            const locations =
+                location === 'all'
+                    ? Object.keys(getPropertyPath(config, 'docker.locations'))
+                    : [location];
 
-        return Promise.all([prefix, config]);
+            return Promise.all([prefix, config, loadState(), locations]);
+        })
+        .then(([prefix, config, state, locations]) => {
+            return Promise.all([
+                state,
+                config,
+                Promise.all(
+                    locations.map(
+                        (loc) =>
+                            new Promise((resolve, reject) => {
+                                const dockerfilePath = getPropertyPath(
+                                    config,
+                                    `docker.locations.${loc}`,
+                                );
 
-    })
-    .then(([prefix, config]) => {
+                                if (!dockerfilePath) {
+                                    return reject(
+                                        new Error(
+                                            `Could not find the ${loc} Docker location.`,
+                                        ),
+                                        false,
+                                        true,
+                                    );
+                                }
 
-        const locations = location === 'all' ? Object.keys(getPropertyPath(config, 'docker.locations')) : [location];
+                                const cmd = `c d build -n ${prefix}/${loc} -t ${tag} ${loc}`;
 
-        return Promise.all([
-            prefix,
-            config,
-            loadState(),
-            locations,
-        ]);
+                                exec(cmd, (err, stdout, stderr) => {
+                                    if ((err || stderr) && stderr) {
+                                        return reject(new Error(stderr));
+                                    }
 
-    })
-    .then(([prefix, config, state, locations]) => {
+                                    if ((err || stderr) && err) {
+                                        return reject(err);
+                                    }
 
-        return Promise.all([
-            state,
-            config,
-            Promise.all(locations.map(loc => new Promise((resolve, reject) => {
+                                    storeState(
+                                        `kubernetes.environments.${state.env}.build.tags.${prefix}/${loc}`,
+                                        tag,
+                                    )
+                                        .then(() => resolve(loc))
+                                        .catch(reject);
+                                });
+                            }),
+                    ),
+                ),
+            ]);
+        })
+        .then(([state, config, locations]) => {
+            // Do we need to build too?
+            if (!program.D) {
+                return Promise.resolve();
+            }
 
-                const dockerfilePath = getPropertyPath(config, `docker.locations.${loc}`);
+            const kubernetesLocations = getPropertyPath(
+                config,
+                `kubernetes.environments.${state.env}.locations`,
+            );
 
-                if (!dockerfilePath) {
-                    return reject(new Error(`Could not find the ${loc} Docker location.`), false, true);
-                }
-
-                const tag = String(Math.floor(Date.now() / 1000));
-                const cmd = `c d build -n ${prefix}/${loc} -t ${tag} ${loc}`;
-
-                exec(cmd, (err, stdout, stderr) => {
-
-                    if ((err || stderr) && stderr) {
-                        return reject(new Error(stderr));
-                    }
-
-                    if ((err || stderr) && err) {
-                        return reject(err);
-                    }
-
-                    storeState(`kubernetes.environments.${state.env}.build.tags.${prefix}/${loc}`, tag)
-                        .then(() => resolve(loc))
-                        .catch(reject);
-
-                });
-
-            }))),
-        ]);
-
-    })
-    .then(([state, config, locations]) => {
-
-        // Do we need to build too?
-        if (!program.D) {
-            return Promise.resolve();
-        }
-
-        const kubernetesLocations = getPropertyPath(config, `kubernetes.environments.${state.env}.locations`);
-
-        locations.forEach((dockerLocation) => {
-
-            return dockerToKubernetesLocation(dockerLocation, kubernetesLocations)
-                .then((kLocation) => {
-
+            locations.forEach((dockerLocation) => {
+                return dockerToKubernetesLocation(
+                    dockerLocation,
+                    kubernetesLocations,
+                ).then((kLocation) => {
                     exec(`c kc deploy ${kLocation.dockerLocation}`);
-
                 });
-
-        });
-
-    })
-    // eslint-disable-next-line handle-callback-err, no-unused-vars, no-empty-function
-    .catch((err) => {});
+            });
+        })
+        // eslint-disable-next-line handle-callback-err, no-unused-vars, no-empty-function
+        .catch((err) => {
+            console.error('ERRRRRRR', err);
+        })
+);
