80345060169937ad4b4f9fef538657a4e0dc29f3
smebberson <scott@scottmebberson.com>
2019-08-08 13:54:32 +0930

    Deprecated the path property used by c d build <location>.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,15 @@
 
 This file is a history of the changes made to @idearium/cli.
 
+## Unreleased
+
+-   Improved command.
+
+### Improved command
+
+-   The path property (from `c.js) used by`c d build <location>` has been deprecated.
+-   `c d build <location>` now prefers `context` and `file` properties, rather than `path`.
+
 ## v3.0.3 (8 August 2019)
 
 -   Bug fixes.
--- bin/c-d-build.js
+++ bin/c-d-build.js
@@ -5,15 +5,29 @@
 const program = require('commander');
 const { exec } = require('shelljs');
 const { basename, resolve } = require('path');
+const { deprecate } = require('util');
 const { loadConfig, newline, reportError } = require('./lib/c');
 
 // The basic program, which uses sub-commands.
 program
     .arguments('<location>')
-    .option('-t [tag]', 'Supply a specific tag, otherwise `latest` will be used')
-    .option('-n [name]', 'Supply a name for the image, otherwise the location name will be used')
-    .description('Provide a Docker location and the Dockerfile will be used to build a Docker image. See https://github.com/idearium/cli#docker-configuration for configuration options.')
+    .option(
+        '-t [tag]',
+        'Supply a specific tag, otherwise `latest` will be used',
+    )
+    .option(
+        '-n [name]',
+        'Supply a name for the image, otherwise the location name will be used',
+    )
+    .description(
+        'Provide a Docker location and the Dockerfile will be used to build a Docker image. See https://github.com/idearium/cli#docker-configuration for configuration options.',
+    )
     .parse(process.argv);
+
+const getPath = deprecate(
+    ({ path }) => path,
+    'The path property has been deprecated in favour of the context property.',
+);
 
 /**
  * Pad a string with a left space, if the string has a length.
@@ -21,13 +35,11 @@ program
  * @return {String} A string left-padded with a space.
  */
 const leftSpace = (str) => {
-
     if (str && typeof str === 'string') {
         return ` ${str}`;
     }
 
     return str;
-
 };
 
 /**
@@ -36,7 +48,6 @@ const leftSpace = (str) => {
  * @return {String} --build-arg arguments for `docker build`.
  */
 const formatBuildArgs = (args = {}) => {
-
     const keys = Object.keys(args);
 
     if (!keys.length) {
@@ -44,62 +55,77 @@ const formatBuildArgs = (args = {}) => {
     }
 
     const buildArguments = keys
-        .map(key => `--build-arg ${key}=${typeof args[key] === 'function' ? args[key]() : args[key] }`)
+        .map(
+            (key) =>
+                `--build-arg ${key}=${
+                    typeof args[key] === 'function' ? args[key]() : args[key]
+                }`,
+        )
         .join(' ');
 
     return `${leftSpace(buildArguments)}`;
-
 };
 
 const [location] = program.args;
 
 if (!location) {
-    return reportError(new Error('You need to provide a Docker location'), program);
+    return reportError(
+        new Error('You need to provide a Docker location'),
+        program,
+    );
 }
 
-return loadConfig('docker.locations')
-    .then((locations) => {
+return loadConfig('docker.locations').then((locations) => {
+    // Make sure we have the locations data.
+    if (!locations) {
+        return reportError(
+            new Error(
+                'Could not find any NPM locations. See https://github.com/idearium/cli#configuration',
+            ),
+            false,
+            true,
+        );
+    }
 
-        // Make sure we have the locations data.
-        if (!locations) {
-            return reportError(new Error('Could not find any NPM locations. See https://github.com/idearium/cli#configuration'), false, true);
+    if (!locations[location]) {
+        return reportError(
+            new Error(`Could not find the ${location} location.`),
+            false,
+            true,
+        );
+    }
+
+    const dockerLocation = locations[location];
+    const context = dockerLocation.context || getPath(dockerLocation);
+    const locationPath = resolve(process.cwd(), context);
+    const { file = 'Dockerfile', useTar = false } = dockerLocation;
+
+    const tag = program.T ? program.T : 'latest';
+    const name = program.N ? program.N : location;
+
+    let cmd = `docker build -t ${name}:${tag}${formatBuildArgs(
+        dockerLocation.buildArgs,
+    )} -f ${file}`;
+
+    if (useTar) {
+        cmd = `tar -chz -C ${locationPath} . | ${cmd} -`;
+    }
+
+    if (!useTar) {
+        cmd = `${cmd} ${locationPath}`;
+    }
+
+    exec(cmd, { silent: program.R }, (err, stdout, stderr) => {
+        if ((err || stderr) && stderr) {
+            return reportError(stderr, false, true);
         }
 
-        if (!locations[location]) {
-            return reportError(new Error(`Could not find the ${location} location.`), false, true);
+        if ((err || stderr) && err) {
+            return reportError(err, false, true);
         }
 
-        const dockerLocation = locations[location];
-        const locationPath = resolve(process.cwd(), dockerLocation.path);
-        const { useTar = false } = dockerLocation;
-
-        const tag = program.T ? program.T : 'latest';
-        const name = program.N ? program.N : location;
-
-        let cmd = `docker build -t ${name}:${tag}${formatBuildArgs(dockerLocation.buildArgs)}`;
-
-        if (useTar) {
-            cmd = `tar -chz -C ${locationPath} . | ${cmd} -`;
+        if (program.R) {
+            process.stdout.write(`${tag}${newline(program.N)}`);
         }
-
-        if (!useTar) {
-            cmd = `${cmd} ${locationPath}`;
-        }
-
-        exec(cmd, { silent: program.R }, (err, stdout, stderr) => {
-
-            if ((err || stderr) && stderr) {
-                return reportError(stderr, false, true);
-            }
-
-            if ((err || stderr) && err) {
-                return reportError(err, false, true);
-            }
-
-            if (program.R) {
-                process.stdout.write(`${tag}${newline(program.N)}`);
-            }
-
-        });
-
     });
+});
