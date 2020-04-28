77d7ef35d20fbfc07c61c4c2ea01471daa772126
allanchau <allan.chau@icloud.com>
2020-04-28 11:26:52 +0930

    use zsh on c mk docker-env

--- bin/c-mk-docker-env.js
+++ bin/c-mk-docker-env.js
@@ -9,8 +9,13 @@ const { spawn } = require('child_process');
 
 // The basic program, which uses sub-commands.
 program
-    .description('Spawns a login shell with your current environment and additional Docker environment variables to use the Minikube\'s Docker daemon')
-    .option('-p [profile]', 'Specify a minikube profile, otherwise the default minikube profile will be used.')
+    .description(
+        "Spawns a login shell with your current environment and additional Docker environment variables to use the Minikube's Docker daemon",
+    )
+    .option(
+        '-p [profile]',
+        'Specify a minikube profile, otherwise the default minikube profile will be used.',
+    )
     .parse(process.argv);
 
 const profile = program.P ? ` --profile ${program.P}` : '';
@@ -21,31 +26,26 @@ Promise.all([
     execa.shell(command),
     // The current shell's environment
     execa.shell('env'),
-])
-    .then(([{ stdout: minikubeStdOut }, { stdout: envStdOut }]) => {
+]).then(([{ stdout: minikubeStdOut }, { stdout: envStdOut }]) => {
+    // Format the Docker environment variables with the current shell's variables
+    const shellExports = minikubeStdOut
+        .split(os.EOL)
+        .filter((line) => line.indexOf('export') === 0)
+        .map((line) => line.substr(7).replace(/"/g, ''))
+        .concat(envStdOut.split(os.EOL));
 
-        // Format the Docker environment variables with the current shell's variables
-        const shellExports = minikubeStdOut
-            .split(os.EOL)
-            .filter(line => line.indexOf('export') === 0)
-            .map(line => line.substr(7).replace(/"/g, ''))
-            .concat(envStdOut.split(os.EOL));
+    // Turn the environment variables into an object
+    const env = {};
 
-        // Turn the environment variables into an object
-        const env = {};
+    shellExports.forEach((line) => {
+        const [name, value] = line.split('=');
 
-        shellExports.forEach((line) => {
-
-            const [name, value] = line.split('=');
-
-            env[name] = value;
-
-        });
-
-        // Create our new login shell
-        spawn('/bin/bash', ['--login'], {
-            env,
-            stdio: 'inherit',
-        });
-
+        env[name] = value;
     });
+
+    // Create our new login shell
+    spawn('/bin/zsh', ['--login'], {
+        env,
+        stdio: 'inherit',
+    });
+});
