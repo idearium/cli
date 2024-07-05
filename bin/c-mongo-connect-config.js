b05a198ac5a480eb3a07e5df9b79cd8a9a9c7bf7
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2024-07-05 15:44:49 +0930

    Refactored c mongo connect in bash and Node.js.

---
+++ bin/c-mongo-connect-config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const { loadConfig } = require('./lib/c');
+const { connectionParts } = require('./lib/c-mongo');
+
+const connectionStringWithAddress = ({
+    address,
+    name,
+    params,
+    password,
+    user,
+}) => {
+    const url = new URL(address);
+
+    url.password = password;
+    url.username = user;
+
+    const cmd = [];
+
+    if (params) {
+        cmd.push(params);
+    }
+
+    return [...cmd, '--uri', `${url.href}/${name}`].join(' ');
+};
+
+const connectionStringWithHost = ({ auth, host, name, params }) => {
+    const cmd = [];
+
+    if (auth) {
+        cmd.push(auth);
+    }
+
+    if (params) {
+        cmd.push(params);
+    }
+
+    return [...cmd, '--host', host, name].join(' ');
+};
+
+(async () => {
+    const [, , env] = process.argv;
+    const details = await loadConfig(`mongo.${env}`);
+    const connection = connectionParts(details);
+    const connectionString = connection.host
+        ? connectionStringWithHost(connection)
+        : connectionStringWithAddress(connection);
+
+    console.log(
+        JSON.stringify({
+            connectionString,
+        })
+    );
+})();
